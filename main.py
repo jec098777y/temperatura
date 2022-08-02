00476c2df4c42543154d6ab7a46814770de4c6ee
jec098777y <106841315+jec098777y@users.noreply.github.com>
2022-08-02 22:40:35 +0200

    Aktualizacja pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 I2C_LCD1602.lcd_init(63)
-
-
+radio.set_transmit_power(7)
+radio.set_group(2)
 def on_forever():
+    temperatura = dstemp.celsius(DigitalPin.P7)
     
-    I2C_LCD1602.show_string("czas", 0, 0)
-            
+    radio.send_string(str(temperatura))
+    I2C_LCD1602.show_string(str(temperatura), 0, 0)
     pause(500)
     pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 I2C_LCD1602.LcdInit(63)
+radio.setTransmitPower(7)
+radio.setGroup(2)
 basic.forever(function on_forever() {
-    I2C_LCD1602.ShowString("czas", 0, 0)
+    let temperatura = dstemp.celsius(DigitalPin.P7)
+    radio.sendString("" + temperatura)
+    I2C_LCD1602.ShowString("" + temperatura, 0, 0)
     pause(500)
     
 })
